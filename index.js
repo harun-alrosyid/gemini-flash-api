8912db27d5c9cf8859483eff5544f6bcf876307b
harun-alrosyid <alrosyid07@gmail.com>
2025-07-28 14:29:11 +0700

    feat: create service genarative from pdf upload

--- index.js
+++ index.js
@@ -1,105 +1,123 @@
-import "dotenv/config"
-import { GoogleGenAI } from "@google/genai"
-import express from "express"
-import multer from "multer"
+import "dotenv/config";
+import { GoogleGenAI } from "@google/genai";
+import express from "express";
+import multer from "multer";
 
-
-const app= express()
-const upload = multer()
-const ai= new GoogleGenAI({apiKey: process.env.GOOGLE_API_KEY})
+const app = express();
+const upload = multer();
+const ai = new GoogleGenAI({ apiKey: process.env.GOOGLE_API_KEY });
 
 // Set Default Model Gemini
-const GEMINI_MODEL= "gemini-2.5-flash"
+const GEMINI_MODEL = "gemini-2.5-flash";
 
-app.use(express.json())
+app.use(express.json());
 
-const PORT= process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
 
-const extractText= (resp) => {
-    try {
-        const text=
-            resp?.response?.candidates?.[0]?.contet?.parts?.[0]?.text ?? 
-            resp?.candidates?.[0]?.content?.parts?.[0]?.text ??
-            resp?.response?.candidates?.[0]?.content?.text
+const extractText = (resp) => {
+  try {
+    const text =
+      resp?.response?.candidates?.[0]?.contet?.parts?.[0]?.text ??
+      resp?.candidates?.[0]?.content?.parts?.[0]?.text ??
+      resp?.response?.candidates?.[0]?.content?.text;
 
-        return text ?? JSON.stringify(resp, null, 2)
+    return text ?? JSON.stringify(resp, null, 2);
+  } catch (error) {
+    console.error("Error Extract file", error);
 
-    } catch (error) {
-        console.error("Error Extract file",error)
-
-        return  JSON.stringify(resp, null, 2)
-    }
-}
+    return JSON.stringify(resp, null, 2);
+  }
+};
 
 app.post("/generate-text", async (req, res) => {
-    
-    try {
+  try {
+    const { prompt } = req.body;
 
-        const {prompt}= req.body
+    const resp = await ai.models.generateContent({
+      model: GEMINI_MODEL,
+      contents: prompt,
+    });
 
-        const resp= await ai.models.generateContent({
-            model: GEMINI_MODEL,
-            contents : prompt,
-        })
-
-        res.json({
-            statusCode: 200,
-            status:"success",
-            result:extractText(resp)
-        })
-        
-    } catch (error) {
-        
-        res.status(500).json({
-            statusCode: 500,
-            status: "error",
-            error: error.message
-        })
-        
-    }
-
-})
+    res.json({
+      statusCode: 200,
+      status: "success",
+      result: extractText(resp),
+    });
+  } catch (error) {
+    res.status(500).json({
+      statusCode: 500,
+      status: "error",
+      error: error.message,
+    });
+  }
+});
 
 app.post("/generate-from-image", upload.single("image"), async (req, res) => {
-    
-    try {
+  try {
+    const { prompt } = req.body;
 
-        const {prompt}= req.body
+    const imageBase64 = req.file.buffer.toString("base64");
 
-        const imageBase64= req.file.buffer.toString("base64")
+    const resp = await ai.models.generateContent({
+      model: GEMINI_MODEL,
+      contents: [
+        { text: prompt },
+        {
+          inlineData: {
+            mimeType: req.file.mimetype,
+            data: imageBase64,
+          },
+        },
+      ],
+    });
 
-        const resp= await ai.models.generateContent({
-            model: GEMINI_MODEL,
-            contents : [
-                {text:prompt},
-                {inlineData:
-                    {
-                        mimeType: req.file.mimetype, 
-                        data: imageBase64
-                    }
-                }
-            
-            ],
-        })
+    res.json({
+      statusCode: 200,
+      status: "success",
+      result: extractText(resp),
+    });
+  } catch (error) {
+    res.status(500).json({
+      statusCode: 500,
+      status: "error",
+      error: error.message,
+    });
+  }
+});
 
-        res.json({
-            statusCode: 200,
-            status:"success",
-            result:extractText(resp)
-        })
-        
-    } catch (error) {
-        
-        res.status(500).json({
-            statusCode: 500,
-            status: "error",
-            error: error.message
-        })
-        
-    }
+app.post("/generate-from-file", upload.single("file"), async (req, res) => {
+  try {
+    const { prompt } = req.body;
 
-})
+    const docsBase64 = req.file.buffer.toString("base64");
+
+    const resp = await ai.models.generateContent({
+      model: GEMINI_MODEL,
+      contents: [
+        { text: prompt ?? "Create a summary of this document" },
+        {
+          inlineData: {
+            mimeType: req.file.mimetype,
+            data: docsBase64,
+          },
+        },
+      ],
+    });
+
+    res.json({
+      statusCode: 200,
+      status: "success",
+      result: extractText(resp),
+    });
+  } catch (error) {
+    res.status(500).json({
+      statusCode: 500,
+      status: "error",
+      error: error.message,
+    });
+  }
+});
