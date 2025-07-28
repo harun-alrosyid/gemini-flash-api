1423c597f12e6187e2e6923771ce3884ad2124a6
harun-alrosyid <alrosyid07@gmail.com>
2025-07-28 16:54:27 +0700

    feat: create gemini sevice

---
+++ src/services/GeminiServices.js
@@ -0,0 +1,20 @@
+import { ai, GEMINI_MODEL } from "../config/geminiClient.js";
+
+export const GeminiService = {
+  async generateText(prompt) {
+    const resp = await ai.models.generateContent({
+      model: GEMINI_MODEL,
+      contents: prompt,
+    });
+    return resp;
+  },
+
+ async generateWithFile(prompt, mimeType, base64 ) {
+    const resp = await ai.models.generateContent({
+      model: GEMINI_MODEL,
+      contents: [{ text: prompt }, { inlineData: { mimeType, data: base64 } }],
+    });
+    return resp;
+  },
+
+};
