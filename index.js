953d5718819533838e711f6bff0388d41c685db8
harun-alrosyid <alrosyid07@gmail.com>
2025-07-28 10:08:25 +0700

    feat: implement extractText function

--- index.js
+++ index.js
@@ -18,3 +18,19 @@ const PORT= process.env.PORT || 3000
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
+
+const extractText= (resp) => {
+    try {
+        const text=
+            resp?.response?.candidates?.[0]?.contenst?.part?.[0]?.text ?? 
+            resp?.candidates?.[0]?.contenst?.part?.[0]?.text ??
+            resp?.response?.candidates?.[0]?.contenst?.text
+
+        return text ?? JSON.stringify(resp, null, 2)
+
+    } catch (error) {
+        console.error("Error Extract file",error)
+
+        return  JSON.stringify(resp, null, 2)
+    }
+}
